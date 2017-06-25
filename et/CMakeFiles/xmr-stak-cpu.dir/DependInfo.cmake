44b1dc69aa2cdd819dbd375cd1810e84455eb4a8
ktypez <keitochan@gmail.com>
2017-06-25 20:43:40 +0000

    changes

---
+++ et/CMakeFiles/xmr-stak-cpu.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/keitochan/xmr-stak-cpu/cli-miner.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/cli-miner.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/console.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/console.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/crypto/cryptonight_common.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/crypto/cryptonight_common.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/executor.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/executor.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/httpd.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/httpd.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/jconf.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/jconf.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/jpsock.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/jpsock.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/minethd.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/minethd.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/socket.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/socket.cpp.o"
+  "/home/keitochan/xmr-stak-cpu/webdesign.cpp" "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-cpu.dir/webdesign.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/keitochan/xmr-stak-cpu/CMakeFiles/xmr-stak-c.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
